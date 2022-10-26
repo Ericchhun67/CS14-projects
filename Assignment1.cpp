c034b5548bc38b2473526ba2fb86ea9636782794
Eric Chhun <57428587+Ericchhun67@users.noreply.github.com>
2022-10-25 21:19:28 -0700

    Add files via upload

---
+++ Assignment1.cpp
@@ -0,0 +1,45 @@
+//
+// Assignment1.cpp
+//
+// Eric Chhun
+// 08/17/22
+// 
+// Assignment1
+// A simple little program to to print and prompt
+//
+
+
+
+
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+        int miles;
+        int feet;
+        double kilo = 2.2;
+
+
+        // prompt for miles and feet
+        cout << "\n Enter miles : " << endl;
+        cin >> miles;
+
+        cout << "\n Enter feet : " << endl;
+        cin >> feet;
+
+
+
+        // make calulations
+        miles = feet * 2;
+        kilo = feet * miles / 3;
+
+        // print results
+        cout << miles << " miles " << " and " << feet << " feet " << " is "
+             << kilo << " kilo." << endl;
+        
+        return 0;
+}
